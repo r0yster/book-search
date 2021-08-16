260c4ba03acf922f2aef6baca4b17190fa0c0dcc
royster <rcantu786@gmail.com>
2021-08-16 01:37:43 -0500

    fixed delete book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -41,21 +41,17 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: String!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
-            email
-            bookCount
-            savedBooks{
-                _id
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
+  }
 `;
